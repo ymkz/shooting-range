feb87d655d6e7b4c3fe14a7777453373c7d19315
ymkz <ymkz@users.noreply.github.com>
2022-02-05 01:41:38 +0900

    chore: simplify example remix cf-workers

--- example-remix-cf-workers/remix.config.js
+++ example-remix-cf-workers/remix.config.js
@@ -2,7 +2,7 @@
  * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
-  appDirectory: "app",
+  appDirectory: "src",
   assetsBuildDirectory: "public/build",
   publicPath: "/build/",
   serverBuildDirectory: "build",
